cb7a39a2163c0d9bc01c1b388b98813f25cdfac4
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-06-10 23:13:54 +0700

    finihsed adding rectangular filter in spatial filter

--- project/filters.py
+++ project/filters.py
@@ -5,6 +5,7 @@ from django.apps import apps
 from django.db.models import Min, Max
 
 from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Polygon
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 
@@ -56,22 +57,7 @@ def table_filter(model_name):
 
 def spatial_filter(model_name):
     'Spatial filter, differ from table filter it uses view merged catalog in database'
-    picked_model = apps.get_model('project', model_name)
-
-    # get the data time scope
-    min_date = picked_model.objects.all().aggregate(min_date=Min('source_origin_dt_init'))['min_date']
-    max_date = picked_model.objects.all().aggregate(max_date=Max('source_origin_dt_init'))['max_date']
-
-    # get the data spacial scope by calculating the median
-    lat_lon = picked_model.objects.values('source_id', 'source_lat_init', 'source_lon_init').distinct()
-    if lat_lon:
-        latitudes = [ loc['source_lat_init'] for loc in lat_lon]
-        longitudes = [loc['source_lon_init'] for loc in lat_lon]
-        median_lat = np.median(latitudes) if latitudes else 0
-        median_lon = np.median(longitudes) if longitudes else 0
-    else:
-        median_lat, median_lon = 0, 0
-    
+    picked_model = apps.get_model('project', model_name)  
 
     class SpatialFilter(django_filters.FilterSet):
         # Date Range Filters
@@ -86,11 +72,11 @@ def spatial_filter(model_name):
                     )
         
         # Circular Radius Filter Fields
-        latitude = NumberFilter(
+        center_latitude = NumberFilter(
             label = "Center Latitude",
         )
 
-        longitude = NumberFilter(
+        center_longitude = NumberFilter(
             label= "Center Longitude",
         )
 
@@ -98,38 +84,73 @@ def spatial_filter(model_name):
             label='Radius (Km)',
         )
 
+        # Rectangular Filter fields
+        min_latitude = NumberFilter(
+            label= "Min Latitude"
+        )
+
+        max_latitude  = NumberFilter(
+            label = "Max Latitude"
+        )
+
+        min_longitude = NumberFilter(
+            label= "Min Longitude"
+        )
+
+        max_longitude = NumberFilter(
+            label= "Max Longitude"
+        )
+
+
         class Meta:
             model = picked_model
-            fields = ['start_date', 'end_date', 'latitude', 'longitude', 'radius']
+            fields = ['start_date', 'end_date',
+                    'center_latitude', 'center_longitude', 'radius'
+                    'min_latitude', 'max_latitude', 'min_longitude', 'max_longitude']
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            self.filters['latitude'].required = False
-            self.filters['longitude'].required = False
+            # Rectangular filter require status
+            self.filters['min_latitude'].required = False
+            self.filters['max_latitude'].required = False 
+            self.filters['min_longitude'].required = False 
+            self.filters['max_longitude'].required = False
+
+            # Circular filter require status
+            self.filters['center_latitude'].required = False
+            self.filters['center_longitude'].required = False
             self.filters['radius'].required = False
         
         def filter_queryset(self, queryset):
+            # Apply base filter before areal filter first
             queryset =  super().filter_queryset(queryset)
 
-            latitude = self.form.cleaned_data.get('latitude')
-            longitude = self.form.cleaned_data.get('longitude')
+            # clean rectangular filter form parameters
+            min_latitude = self.form.cleaned_data.get('min_latitude')
+            max_latitude = self.form.cleaned_data.get('max_latitude')
+            min_longitude = self.form.cleaned_data.get('min_longitude')
+            max_longitude = self.form.cleaned_data.get('max_longitude')
+
+            # clean circular filter form parameters
+            center_latitude = self.form.cleaned_data.get('center_latitude')
+            center_longitude = self.form.cleaned_data.get('center_longitude')
             radius = self.form.cleaned_data.get('radius')
-        
-            if all (v is not None for v in [latitude, longitude, radius]):
-                try:
+
+
+            try:
+                if all (v is not None for v in [center_latitude, center_longitude, radius]):
                     # validate form filter input
-                    if not (-90 <= latitude <=90 ):
-                        logger.error(f"Invalid latitude: {latitude}")
+                    if not (-90 <= center_latitude <=90 ):
+                        logger.error(f"Invalid latitude: {center_latitude}")
                         return queryset
-                    if not(-180 <= longitude <=180):
-                        logger.error(f"Invalid longitude: {longitude}")
+                    if not(-180 <= center_longitude <=180):
+                        logger.error(f"Invalid longitude: {center_longitude}")
                     if radius < 0:
                         logger.error(f"Invalid radius: {radius}")
                         return queryset
                     
                     # create central point for reference
-                    center_point = Point(longitude, latitude, srid=4326)
-
+                    center_point = Point(center_longitude, center_latitude, srid=4326)
                     queryset = queryset.filter(
                         location_init_isnull = False
                         ).filter(
@@ -137,9 +158,36 @@ def spatial_filter(model_name):
                         ).annotate(
                             distance = Distance('location_init', center_point)
                         )
+                
+                # Apply rectangular filter if the circular filter is not used.
+                elif all ( v is not None for v in [min_latitude, max_latitude, min_longitude, max_longitude]):
+                    if not (-90 <= min_latitude <= max_latitude <= 90):
+                        logger.error(f"Invalid latitude range: min={min_latitude}, max={max_latitude}")
+                        return queryset
+                    if not (-180 <= min_longitude <= max_longitude <=180):
+                        logger.error(f"Invalid longitude range: min={min_longitude}, max={max_longitude}")
+                        return queryset
 
-                except Exception as e:
-                    logger.error(f"Spatial filter error: {str(e)}")
+                    # create polygon for the rectangular bounding box
+                    bbox = Polygon (
+                        (
+                            (min_longitude, min_latitude),
+                            (max_longitude, min_latitude),
+                            (max_longitude, max_latitude),
+                            (max_longitude, min_latitude),
+                            (min_longitude,  min_latitude)
+                        ),
+                        srid= 4326
+                    )
+                    queryset = queryset.filter(
+                        location_init_isnull = False
+                    ).filter(
+                        location_init_within = bbox
+                    )
+            except Exception as e :
+                logger.error(f"Spatial filter error: {str(e)}")
+                return queryset
+            
             return queryset
         
     return SpatialFilter
