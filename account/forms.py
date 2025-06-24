6a4a4cc67428d83dc87eb74caa34d97e284dcb0d
bgjx <bgjx.edelos@gmail.com>
2025-06-24 22:39:41 +0700

    improve form

--- account/forms.py
+++ account/forms.py
@@ -21,26 +21,38 @@ class LoginForm(forms.Form):
     )
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Username'
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class':'form-control',
+                'placeholder': 'Password'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class':'form-control',
+                'placeholder': 'Confirm Password'
+            }
+        )
+    )
+    email = forms.CharField(
+        widget= forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Email Address'
+            }
+        )
+    )
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
-        widgets = {
-            'username': forms.TextInput(
-                attrs= {
-                    'class': 'form-control',
-                    'placeholder': 'Username'
-                }
-            ),
-            'password1': forms.PasswordInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder':'Password'
-                }
-            ),
-            'password2': forms.PasswordInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder':'Confirm Password'
-                }
-            ),
-        }
+        fields = ['username', 'password1', 'password2', 'email']
