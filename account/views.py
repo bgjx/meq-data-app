a48e0f91d0054daf99cc63f5ea4f323e82502dc7
bgjx <bgjx.edelos@gmail.com>
2025-06-24 22:30:11 +0700

    create views for login singup

--- account/views.py
+++ account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import Group
+from .forms import LoginForm, SignupForm
+
+
