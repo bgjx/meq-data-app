7e920868631bcdcaf2276c0a685e3a371cf111fb
bgjx <bgjx.edelos@gmail.com>
2025-06-24 18:27:07 +0700

    setting all urls and view for account

---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login_view'),
+    path('signup/', views.signup_view, name='signup_view')
+    
+]
