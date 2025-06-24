dbaf189c9f4282a2d0c0db23cf6d6d406943efc1
bgjx <bgjx.edelos@gmail.com>
2025-06-25 01:45:24 +0700

    pending apply models

--- account/models.py
+++ account/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
-    is_admin = models.BooleanField('Is admin', default=False)
-    is_guest = models.BooleanField('Is guest', default=False)
+# class User(AbstractUser):
+#     is_admin = models.BooleanField('Is admin', default=False)
+#     is_guest = models.BooleanField('Is guest', default=False)
