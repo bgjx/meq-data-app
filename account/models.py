1f7c61d66a3d26e8cacc20ef1677088c612f455b
bgjx <bgjx.edelos@gmail.com>
2025-06-25 11:12:14 +0700

    add userprofile model, extend User auth based model for email verification

--- account/models.py
+++ account/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-# Create your models here.
-# class User(AbstractUser):
-#     is_admin = models.BooleanField('Is admin', default=False)
-#     is_guest = models.BooleanField('Is guest', default=False)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_email_verified= models.BooleanField(default=False)
+    organization = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
