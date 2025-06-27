0f2f31e4cb3f3e0e7ed15c593bcdffaf7648d992
bgjx <bgjx.edelos@gmail.com>
2025-06-28 00:10:41 +0700

    fix verification templates

--- django_project/account/urls.py
+++ django_project/account/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name = 'account/password_reset_done.html'
         ), name='password_reset_done'),
-    path('reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='account/password_reset_confirm.html'
     ), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
