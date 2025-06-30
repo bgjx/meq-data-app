f4da2392bc8f2bd4d4ab79d51919f10222f9912a
bgjx <bgjx.edelos@gmail.com>
2025-06-30 13:49:48 +0700

    comment out urls for both meq_maps and get_meq_map

--- django_project/project/urls.py
+++ django_project/project/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
 
     path('<slug:site_slug>/download/<str:catalog_type>', views.download_catalog, name='download-catalog'),
 
-    path('<slug:site_slug>/maps', views.meq_maps, name='meq-maps'),
+    # path('<slug:site_slug>/maps', views.meq_maps, name='meq-maps'),
 
-    path('<slug:site_slug>/get-meq-data', views.get_meq_data, name='get-meq-data'),
+    # path('<slug:site_slug>/get-meq-data', views.get_meq_data, name='get-meq-data'),
 
     path('<slug:site_slug>/data-analysis', views.data_analysis, name='data-analysis'),
 
