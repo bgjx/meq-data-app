803ac7b8cb5a124ee8dabd4c65a7fdcb7131ec00
bgjx <bgjx.edelos@gmail.com>
2025-06-30 13:47:07 +0700

    initiate picking catalog table

--- django_project/project/views.py
+++ django_project/project/views.py
@@ -48,15 +48,15 @@ def project_site(request, site_slug = None):
     return render(request, 'project/data-explore.html', context)
 
 
-def meq_maps(request, site_slug = None):
-    'Generate map frame for meq distributions map'
-    site = get_object_or_404(Site, slug=site_slug)
-    mapbox_access_token = MAPBOX_API_TOKEN
-    context = {
-        'site': site,
-        'MAPBOX_TOKEN': mapbox_access_token,
-    }
-    return render(request, 'project/event-distributions.html', context=context)
+# def meq_maps(request, site_slug = None):
+#     'Generate map frame for meq distributions map'
+#     site = get_object_or_404(Site, slug=site_slug)
+#     mapbox_access_token = MAPBOX_API_TOKEN
+#     context = {
+#         'site': site,
+#         'MAPBOX_TOKEN': mapbox_access_token,
+#     }
+#     return render(request, 'project/event-distributions.html', context=context)
 
 
 def data_analysis(request, site_slug = None):
@@ -83,51 +83,51 @@ def data_analysis(request, site_slug = None):
     return render(request, 'project/data-analysis.html', context)
 
 
-#  API endpoint function
-def get_meq_data(request, site_slug = None):
-    'API endpoint to get hypocenter data and calculate the center point'
+# #  API endpoint function
+# def get_meq_data(request, site_slug = None):
+#     'API endpoint to get hypocenter data and calculate the center point'
 
-    site = get_object_or_404(Site, slug=site_slug)    
-    # map center
-    center_map = {
-        'seml': {'lat': -1.616487, 'lon':101.137171},
-        'serd': {'lat': -4.220185, 'lon': 103.379187}
-    }
+#     site = get_object_or_404(Site, slug=site_slug)    
+#     # map center
+#     center_map = {
+#         'seml': {'lat': -1.616487, 'lon':101.137171},
+#         'serd': {'lat': -4.220185, 'lon': 103.379187}
+#     }
 
-    # define catalog type
-    catalog_types = [
-        {'type': 'relocated'},
-        {'type': 'initial'}
-    ]
+#     # define catalog type
+#     catalog_types = [
+#         {'type': 'relocated'},
+#         {'type': 'initial'}
+#     ]
     
-    # get station dataframe
-    db_station = get_station('project', site_slug)
-    df_station = pd.DataFrame(list(db_station.values()))
+#     # get station dataframe
+#     db_station = get_station('project', site_slug)
+#     df_station = pd.DataFrame(list(db_station.values()))
 
-    data = {
-        'station': df_station.to_dict(orient='records'),
-        'center_map': center_map[site_slug]
-    }
+#     data = {
+#         'station': df_station.to_dict(orient='records'),
+#         'center_map': center_map[site_slug]
+#     }
 
-    # get hypocenter dataframe and normalize magnitude 
-    for catalog in catalog_types:
-        catalog_type = catalog['type']
-        db_table, model = get_hypocenter_catalog('project', site_slug, catalog_type)
-        df_meq = pd.DataFrame(list(db_table.values()))
-        df_meq = df_meq[['source_id', 'source_lat', 'source_lon', 'source_depth_m', 'magnitude']]
+#     # get hypocenter dataframe and normalize magnitude 
+#     for catalog in catalog_types:
+#         catalog_type = catalog['type']
+#         db_table, model = get_hypocenter_catalog('project', site_slug, catalog_type)
+#         df_meq = pd.DataFrame(list(db_table.values()))
+#         df_meq = df_meq[['source_id', 'source_lat', 'source_lon', 'source_depth_m', 'magnitude']]
         
-        # magnitude normalization
-        average_magnitude = df_meq.magnitude.median()
-        df_meq['magnitude'] = df_meq['magnitude'].fillna(average_magnitude)
+#         # magnitude normalization
+#         average_magnitude = df_meq.magnitude.median()
+#         df_meq['magnitude'] = df_meq['magnitude'].fillna(average_magnitude)
 
-        min_magnitude = df_meq.magnitude.min()
-        normalized_magnitude = [1 * (( -1 * min_magnitude) + data) for data in list(df_meq.magnitude)]
-        df_meq['norm_magnitude'] = normalized_magnitude
+#         min_magnitude = df_meq.magnitude.min()
+#         normalized_magnitude = [1 * (( -1 * min_magnitude) + data) for data in list(df_meq.magnitude)]
+#         df_meq['norm_magnitude'] = normalized_magnitude
 
-        # update data object
-        data[f'meq_{catalog_type}'] = df_meq.to_dict(orient='records')
+#         # update data object
+#         data[f'meq_{catalog_type}'] = df_meq.to_dict(orient='records')
 
-    return JsonResponse(data)
+#     return JsonResponse(data)
 
 
 def get_analysis_data( request, site_slug=None):
