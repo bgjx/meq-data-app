34535b280ac655a74a5d33d8cfed40160f834661
bgjx <bgjx.edelos@gmail.com>
2025-07-05 20:17:40 +0700

    latest update seciton added

--- django_project/frontpage/views.py
+++ django_project/frontpage/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 from frontpage.models import Site
+from project.models import Updates
 from frontpage.decorators import guest_or_admin_required
 
 def projects(request):
@@ -12,5 +13,12 @@ def projects(request):
 @login_required
 @guest_or_admin_required
 def frontpage(request):
-    context={'mapbox_access_token' : settings.MAPBOX_API_TOKEN}
+    # Query new updates
+    latest_updates = Updates.objects.order_by('-updated_at')[:5]
+
+    context={
+        'mapbox_access_token' : settings.MAPBOX_API_TOKEN,
+        'latest_updates': latest_updates
+    }
+    
     return render(request, 'frontpage/frontpage.html', context=context)
