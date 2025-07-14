561a7a88c289c5bb079a556e1e2163a6b6efb7f1
bgjx <bgjx.edelos@gmail.com>
2025-07-15 01:27:23 +0700

    updates utils doc in frontpage app

--- django_project/frontpage/utils.py
+++ django_project/frontpage/utils.py
@@ -1,6 +1,16 @@
 from django.apps import apps
 
 def get_updates(app_label:str) -> str:
+    """
+    Retrieve the name of the model corresponding to a specific database table.
+
+    Args:
+        app_label (str): The name of the Django app.
+
+    Returns:
+        str: The name of the model if it corresponds to the 'project_updates' 
+             table, or None if not found.
+    """
     table_name = 'project_updates'
     models = apps.get_app_config(app_label).get_models() 
     for model in models:
