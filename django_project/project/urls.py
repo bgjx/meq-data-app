8a7128e45e53a1273f2bfac7c5d394780d0eca67
bgjx <bgjx.edelos@gmail.com>
2025-08-07 18:18:03 +0700

    fix url naming

--- django_project/project/urls.py
+++ django_project/project/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
 
     path('<slug:site_slug>/data-analysis', views.data_analysis, name='data-analysis'),
 
-    path('<slug:site_slug>/get-analysis-data', views.analysis_data, name='get-analysis-data'),
+    path('<slug:site_slug>/analysis-data', views.analysis_data, name='analysis-data'),
 ]
