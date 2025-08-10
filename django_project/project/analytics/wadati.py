bca677383064b5cfa0bbd59503ca75f254d3ab12
bgjx <bgjx.edelos@gmail.com>
2025-08-10 16:58:58 +0700

    imporve wadati docstring

--- django_project/project/analytics/wadati.py
+++ django_project/project/analytics/wadati.py
@@ -1,11 +1,27 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
-
 from typing import Dict
 
+def compute_wadati_profile(picking_df: pd.DataFrame) -> Dict[str, list]:
+    """
+    Computes the Wadati profile for a given DataFrame containing seismic phase picking information.
 
-def compute_wadati_profile(picking_df:pd.DataFrame) -> Dict:
+    The function calculates the origin time for each unique source_id based on the arrival times 
+    of seismic phases and their respective Ts and Tp values. It uses a linear fit to determine the 
+    origin time and calculates the P-wave travel times.
+
+    Args:
+        picking_df (pd.DataFrame): A DataFrame containing the following columns:
+            - source_id (str): Unique identifier for each seismic source.
+            - p_arrival_dt (datetime): Arrival dates and times of seismic phases.
+            - Ts_Tp (float): Values associated with the phase arrivals used for calculations.
+
+    Returns:
+        Dict[str, list]: A dictionary containing:
+            - 'p_travel' (list): List of calculated P-wave travel times for each phase.
+            - 'ts_tp' (list): List of Ts_Tp values corresponding to the computed travel times.
+        """
     epoch = datetime(1970, 1, 1)
 
     origin_time = {
@@ -14,9 +30,9 @@ def compute_wadati_profile(picking_df:pd.DataFrame) -> Dict:
     }
 
     for sid in picking_df['source_id'].unique():
-        phase_df = picking_df[picking_df['source_id'] == id].copy()
+        phase_df = picking_df[picking_df['source_id'] == sid].copy()
         phase_df['epoch_delta'] = phase_df['p_arrival_dt'].apply(
-            lambda x : (x - epoch).total_seconds()
+            lambda x: (x - epoch).total_seconds()
         )
 
         # calculate the t_0 with np.polyfit
@@ -30,8 +46,8 @@ def compute_wadati_profile(picking_df:pd.DataFrame) -> Dict:
     origin_time_df = pd.DataFrame.from_dict(origin_time)
     merged = picking_df.merge(
         origin_time_df,
-        on = 'source_id',
-        how ='inner'
+        on='source_id',
+        how='inner'
     )
     merged['p_travel'] = (merged['p_arrival_dt'] - merged['origin_time']).dt.total_seconds()
 
