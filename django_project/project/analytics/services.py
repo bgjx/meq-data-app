2555fd2cd4587530103f962dd09bb631463c91ac
bgjx <bgjx.edelos@gmail.com>
2025-08-11 18:15:01 +0700

    implement analhytics service to api

--- django_project/project/analytics/services.py
+++ django_project/project/analytics/services.py
@@ -26,3 +26,23 @@ def analyze_general_performance(queryset, site_slug:str) -> Dict[str, Dict[str, 
         'overall_daily_intensities': compute_overall_daily_intensities(hypocenter_df),
         'hypocenter': retrieve_catalog_hypocenter(hypocenter_df, picking_df, site_slug)
         }
+
+
+def analyze_detail_analytics(queryset, site_slug:str) -> Dict[str, Dict[str, list]]:
+
+    df = pd.DataFrame.from_records(queryset.values())
+    validate_dataframe(df)
+
+    hypocenter_df, picking_df = preprocess_dataframe(df)
+    magnitude_series = hypocenter_df['magnitude'].dropna()
+
+    return {
+        'time_series_performance': compute_time_series_performance(picking_df),
+        'station_performance': compute_station_performance(picking_df),
+        'wadati_profile': compute_wadati_profile(picking_df),
+        'hypocenter': retrieve_catalog_hypocenter(hypocenter_df, picking_df, site_slug),
+        'gap_histogram': {'gap': hypocenter_df['gap_init'].dropna().tolist()},
+        'rms_error': compute_hypocenter_rms_error(hypocenter_df),
+        'magnitude_histogram': {'magnitude':magnitude_series.tolist()},
+        'gutenberg_analysis': gutenberg_analysis(magnitude_series)
+        }
--- django_project/project/views/api/api.py
+++ django_project/project/views/api/api.py
@@ -7,17 +7,12 @@ from django.apps import apps
 import pandas as pd
 
 from project.filters import spatial_filter
-from project.analytics.base import validate_dataframe, preprocess_dataframe
-from project.analytics.general import (
-    compute_general_statistics,
-    compute_overall_daily_intensities,
-    compute_station_performance,
-    compute_time_series_performance,
-    retrieve_catalog_hypocenter,
-    compute_hypocenter_rms_error
+
+from project.analytics.services import (
+    analyze_general_performance,
+    analyze_detail_analytics
 )
-from project.analytics.wadati import compute_wadati_profile
-from project.analytics.gutenberg import gutenberg_analysis
+
 from project.utils import get_merged_catalog
 
 
@@ -31,19 +26,13 @@ class GeneralPerformanceAPIView(APIView):
 
         filter_class = spatial_filter(model)
         filter_instance = filter_class(request.GET, queryset=get_model.objects.all())
-        queryset = filter_instance.qs 
+        queryset = filter_instance.qs
 
-        df = pd.DataFrame.from_records(queryset.values())
+        try:
+            data = analyze_general_performance(queryset, site_slug)
+        except ValueError as e:
+            return Response({"error":str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-        validate_dataframe(df)
-
-        hypocenter_df, picking_df = preprocess_dataframe(df)
-
-        data = {
-            'general_statistics': compute_general_statistics(hypocenter_df, picking_df),
-            'overall_daily_intensities': compute_overall_daily_intensities(hypocenter_df),
-            'hypocenter': retrieve_catalog_hypocenter(hypocenter_df, picking_df, site_slug)
-        }
 
         return Response(data, status=status.HTTP_200_OK)
 
@@ -60,23 +49,10 @@ class DetailAnalyticsAPIView(APIView):
         filter_instance = filter_class(request.GET, queryset=get_model.objects.all())
         queryset = filter_instance.qs
 
-        df = pd.DataFrame.from_records(queryset.values())
-
-        validate_dataframe(df)
-        
-        hypocenter_df, picking_df = preprocess_dataframe(df)
-        magnitude_series = hypocenter_df['magnitude'].dropna()
-
-        data = {
-            'time_series_performance': compute_time_series_performance(picking_df),
-            'station_performance': compute_station_performance(picking_df),
-            'wadati_profile': compute_wadati_profile(picking_df),
-            'hypocenter': retrieve_catalog_hypocenter(hypocenter_df, picking_df, site_slug),
-            'gap_histogram': {'gap': hypocenter_df['gap_init'].dropna().tolist()},
-            'rms_error': compute_hypocenter_rms_error(hypocenter_df),
-            'magnitude_histogram': {'magnitude':magnitude_series.tolist()},
-            'gutenberg_analysis': gutenberg_analysis(magnitude_series)
-        }
+        try:
+            data = analyze_detail_analytics(queryset, site_slug)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(data, status=status.HTTP_200_OK)
 
